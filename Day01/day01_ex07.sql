8709a49efb596650e15b6a38beb417e8dc6f4c39
Gurgen Osipyan <osipyangurgen09@gmail.com>
2024-03-14 14:16:33 +0300

    formatted the day01_ex07.sql

--- Day01/day01_ex07.sql
+++ Day01/day01_ex07.sql
@@ -1,5 +1,5 @@
-select order_date, concat(person.name,' (age:',person.age,')') as person_information
+select order_date, concat(person.name, ' (age:', person.age, ')') as person_information
 from person_order
          join person
               on person_order.person_id = person.id
-order by order_date,person_information
+order by order_date, person_information
